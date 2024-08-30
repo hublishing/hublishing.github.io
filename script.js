79329971d3af70d8939ffdd366744b386063be01
hublishing <jaehun_@kakao.com>
2024-08-30 12:46:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function fetchPokemonList() {
     const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=151'; // 첫 번째 세대 포켓몬 151개를 가져옵니다.
     fetch(apiUrl)
         .then(response => {
+            console.log('Response Status:', response.status);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         })
         .then(data => {
+            console.log('Data fetched:', data); // 데이터가 제대로 받아졌는지 로그 출력
             const pokemonList = document.getElementById('pokemonList');
             if (data.results) {
                 data.results.forEach(pokemon => {
